e81c0ce7cd6d7a2fd9c9a9ad8104f84d75f5d844
York_ruve <jorgeandres.ruavera@gmail.com>
2025-01-14 16:25:43 -0500

    :wrench: Actualización del Gradle: Incremento de minSdkVersion a 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.york_ruve.quickbitemaneger"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
