593b5b2a0b1c40101d632622495c3b83b988a577
York_ruve <jorgeandress.ruavera@gmail.com>
2025-03-22 15:17:35 -0500

    :sparkles: Creacion e Implepmentacion del adapter del recyclerView

---
+++ app/src/main/java/com/york_ruve/quickbitemaneger/Presentation/ViewModels/ingredientAdapter.kt
@@ -0,0 +1,33 @@
+package com.york_ruve.quickbitemaneger.Presentation.ViewModels
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.york_ruve.quickbitemaneger.Domain.Model.Ingredients
+import com.york_ruve.quickbitemaneger.R
+import com.york_ruve.quickbitemaneger.databinding.ItemIngredientsBinding
+
+class ingredientAdapter(private val ingredientlist:List<Ingredients>): RecyclerView.Adapter<ingredientAdapter.ViewHolder>() {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val binding = ItemIngredientsBinding.bind(view)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ingredientAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_ingredients, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ingredientAdapter.ViewHolder, position: Int) {
+        val ingredient = ingredientlist[position]
+        holder.binding.tvName.text = ingredient.name
+        holder.binding.tvQuatity.text = ingredient.stock.toString()
+        holder.binding.tvUnit.text = ingredient.unit
+    }
+
+    override fun getItemCount(): Int = ingredientlist.size
+
+}
--- app/src/main/java/com/york_ruve/quickbitemaneger/Presentation/Views/InventoryFragment.kt
+++ app/src/main/java/com/york_ruve/quickbitemaneger/Presentation/Views/InventoryFragment.kt
@@ -5,22 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.york_ruve.quickbitemaneger.Presentation.ViewModels.ingredientAdapter
+import com.york_ruve.quickbitemaneger.Presentation.ViewModels.ingredientViewModel
 import com.york_ruve.quickbitemaneger.R
 import com.york_ruve.quickbitemaneger.databinding.FragmentInventoryBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class InventoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentInventoryBinding
+
+    private val inventoryViewModel: ingredientViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentInventoryBinding.inflate(inflater, container, false)
+        binding = FragmentInventoryBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
 
+    private fun initRecyclerView() {
+        inventoryViewModel.getAllIngredients()
+        inventoryViewModel.ingredients.observe(viewLifecycleOwner) {
+            binding.rvIngredients.layoutManager = LinearLayoutManager(this.context)
+            binding.rvIngredients.adapter = ingredientAdapter(it)
+        }
     }
 
 }
