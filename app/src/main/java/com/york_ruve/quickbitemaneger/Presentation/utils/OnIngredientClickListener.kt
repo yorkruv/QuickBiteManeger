9e638319807e9f8f09774f3a929b1f676483334d
York_ruve <jorgeandress.ruavera@gmail.com>
2025-03-22 19:27:42 -0500

    :hammer: Actualizacion de OnIngredientClickListener.kt

--- app/src/main/java/com/york_ruve/quickbitemaneger/Presentation/utils/OnIngredientClickListener.kt
+++ app/src/main/java/com/york_ruve/quickbitemaneger/Presentation/utils/OnIngredientClickListener.kt
@@ -1,6 +1,8 @@
 package com.york_ruve.quickbitemaneger.Presentation.utils
 
+import com.york_ruve.quickbitemaneger.Domain.Model.Ingredients
+
 interface OnIngredientClickListener {
-    fun onEditIngredientClick(ingredientId: Int)
+    fun onEditIngredientClick(ingredient: Ingredients)
     fun onDeleteIngredientClick(ingredientId: Int)
 }
