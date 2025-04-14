2d34e419880ea52f929c31789d44deef216feec0
York_ruve <jorgeandress.ruavera@gmail.com>
2025-04-14 03:19:43 -0500

    :wrench: Cambio de nombre del archivo de application

---
+++ app/src/main/java/com/york_ruve/quickbitemaneger/di/MyApplication.kt
@@ -0,0 +1,174 @@
+package com.york_ruve.quickbitemaneger.di
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.appopen.AppOpenAd
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
+
+private const val LOG_TAG = "AppOpenAdManager"
+private const val AD_UNIT_ID = "ca-app-pub-3940256099942544/9257395921"
+
+@HiltAndroidApp
+class MyApplication : Application(), Application.ActivityLifecycleCallbacks, DefaultLifecycleObserver {
+
+    private lateinit var appOpenAdManager: AppOpenAdManager
+    private var currentActivity: Activity? = null
+
+    override fun onCreate() {
+        super<Application>.onCreate()
+        registerActivityLifecycleCallbacks(this)
+        MobileAds.initialize(this)
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+        appOpenAdManager = AppOpenAdManager()
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        currentActivity?.let {
+            appOpenAdManager.showAdIfAvailable(it, object : OnShowAdCompleteListener {
+                override fun onShowAdComplete() {
+                    // No hacemos nada porque el usuario volverá a la actividad que muestra el anuncio
+                }
+            })
+        }
+    }
+
+    interface OnShowAdCompleteListener {
+        fun onShowAdComplete()
+    }
+
+    private class AppOpenAdManager() {
+        private var appOpenAd: AppOpenAd? = null
+        private var isLoadingAd = false
+        var isShowingAd = false
+
+        /** Request an ad. */
+        fun loadAd(context: Context) {
+            // Do not load ad if there is an unused ad or one is already loading.
+            if (isLoadingAd || isAdAvailable()) {
+                return
+            }
+
+            isLoadingAd = true
+            val request = AdRequest.Builder().build()
+            AppOpenAd.load(
+                context, AD_UNIT_ID, request,
+                object : AppOpenAd.AppOpenAdLoadCallback() {
+
+                    override fun onAdLoaded(ad: AppOpenAd) {
+                        // Called when an app open ad has loaded.
+                        Log.d(LOG_TAG, "Ad was loaded.")
+                        appOpenAd = ad
+                        isLoadingAd = false
+                    }
+
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        // Called when an app open ad has failed to load.
+                        Log.d(LOG_TAG, loadAdError.message)
+                        isLoadingAd = false;
+                    }
+                })
+        }
+
+        fun showAdIfAvailable(
+            activity: Activity,
+            onShowAdCompleteListener: OnShowAdCompleteListener) {
+            // If the app open ad is already showing, do not show the ad again.
+            if (isShowingAd) {
+                Log.d(LOG_TAG, "The app open ad is already showing.")
+                return
+            }
+
+            // If the app open ad is not available yet, invoke the callback then load the ad.
+            if (!isAdAvailable()) {
+                Log.d(LOG_TAG, "The app open ad is not ready yet.")
+                onShowAdCompleteListener.onShowAdComplete()
+                loadAd(activity)
+                return
+            }
+
+            appOpenAd?.setFullScreenContentCallback(
+                object : FullScreenContentCallback() {
+
+                    override fun onAdDismissedFullScreenContent() {
+                        // Called when full screen content is dismissed.
+                        // Set the reference to null so isAdAvailable() returns false.
+                        Log.d(LOG_TAG, "Ad dismissed fullscreen content.")
+                        appOpenAd = null
+                        isShowingAd = false
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        loadAd(activity)
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                        // Called when fullscreen content failed to show.
+                        // Set the reference to null so isAdAvailable() returns false.
+                        Log.d(LOG_TAG, adError.message)
+                        appOpenAd = null
+                        isShowingAd = false
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        loadAd(activity)
+                    }
+
+                    override fun onAdShowedFullScreenContent() {
+                        // Called when fullscreen content is shown.
+                        Log.d(LOG_TAG, "Ad showed fullscreen content.")
+                    }
+                })
+            isShowingAd = true
+            appOpenAd?.show(activity)
+        }
+
+        /** Check if ad exists and can be shown. */
+        private fun isAdAvailable(): Boolean {
+            return appOpenAd != null
+        }
+    }
+
+    override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        currentActivity = activity
+    }
+
+    override fun onActivityResumed(p0: Activity) {
+
+    }
+
+    override fun onActivityPaused(p0: Activity) {
+
+    }
+
+    override fun onActivityStopped(p0: Activity) {
+
+    }
+
+    override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+
+    }
+
+    override fun onActivityDestroyed(p0: Activity) {
+
+    }
+
+
+}
